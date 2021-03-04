5f8e82e8198ab41c3bb85411e7feb2d697da4a77
EmperorParzival <29981946+EmperorParzival@users.noreply.github.com>
2021-03-03 20:51:59 -0500

    fix: Remove old commented code

--- src/hentai/main.rs
+++ src/hentai/main.rs
@@ -1,6 +1,5 @@
-use clap::{clap_app, crate_authors, crate_description, crate_version};
+// use clap::{clap_app, crate_authors, crate_description, crate_version};
 use hentai::{Hentai, Result, Website};
-use std::{env, path};
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -18,34 +17,6 @@ async fn main() -> Result<()> {
     //     )
     // )
     // .get_matches();
-
-    // println!("{:?}", matches.subcommand_matches("download"));
-    // let doujin = if let Some(ref matches) = matches.subcommand_matches("download") {
-    //     let domain = if matches.is_present("domain") {
-    //         Website::XXX
-    //     } else {
-    //         Website::NET
-    //     };
-
-    //     println!("got here");
-    //     let _directory = if let Some(target) = matches.value_of("target") {
-    //         path::PathBuf::from(target)
-    //     } else {
-    //         env::current_dir()?
-    //     };
-
-    //     println!("here");
-    //     if let Some(code) = matches.value_of("code") {
-    //         Hentai::new(code.parse::<u32>().unwrap(), domain).await?
-    //     } else {
-    //         Hentai::random(domain).await?
-    //     };
-    // };
-
-    // println!("hi {:?}", doujin);
-
-    // let response = Hentai::new(165961, Website::NET).await?;
-    // println!("{:?}", response); // makes use of the Debug trait on Hentai
 
     let response = Hentai::random(Website::XXX).await?;
     println!("{:?}", response);
